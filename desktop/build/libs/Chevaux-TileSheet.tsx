17ce75fbe9c975e1639a08060bdd3802a0dfedcd
Brieuc_Hemez <brieuchemez@gmail.com>
2021-01-11 01:45:14 +0100

    build

---
+++ desktop/build/libs/Chevaux-TileSheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Chevaux-TileSheet" tilewidth="16" tileheight="16" tilecount="252" columns="9">
+ <image source="Chevaux-TileSheet.png" width="144" height="448"/>
+</tileset>
